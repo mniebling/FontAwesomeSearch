f7f725333dff46656f1ff6b9fdd9ff5542f392dd
Mike Niebling <michael.niebling@gmail.com>
2013-05-28 21:17:35 -0500

    Modify default state (and other changes)
    Remove code to hide icons initially; fix jump when scroll bar appears;
    add autofocus attribute; add license

--- scripts/fontawesome-search.js
+++ scripts/fontawesome-search.js
@@ -11,36 +11,23 @@
 		$('.results').html(window.Mustache.render(tmpl, iconJSON));
 
 
-		// To begin, hide 'em all
-		$('li').hide();
-
 
 		// I guess I'll write my own jankety typeahead
 		$('#input-search').keyup(function () {
 
 			var target = $('#input-search').val();
 
-			// Only do stuff if there's 2+ characters typed in,
-			// to avoid flashing the entire list when the user first starts typing
-			if (target.length > 1) {
+			// Check all the li's to see if they match the search
+			// If not, hide them 
+			$('li').each(function (index, value) {
 
-				// Check all the li's to see if they match the search
-				// If so, show them 
-				$('li').each(function (index, value) {
-
-					if($(this).attr('data-icon-name').indexOf(target) != -1) {
-						$(this).fadeIn('fast');
-					}
-					else {
-						$(this).fadeOut('fast');
-					}
-				});
-			}
-
-			// Otherwise, there's 0 or 1 characters so hide all the results
-			else {
-				$('li').fadeOut('fast');
-			}
+				if($(this).attr('data-icon-name').indexOf(target) != -1) {
+					$(this).fadeIn('fast');
+				}
+				else {
+					$(this).fadeOut('fast');
+				}
+			});
 
 		});
 	});
