3038378e66b2e022cf89842c401db1f8a2bb0fd2
Mike Niebling <michael.niebling@gmail.com>
2016-02-17 21:17:52 -0600

    Remove redundant declaration

--- tasks/icons-etl.js
+++ tasks/icons-etl.js
@@ -8,14 +8,14 @@ yaml    = require('yamljs')
 // Fetch the icons.yml file from the Github repo. This step is necessary
 // because the file is ignored in the npm module, so we don't have it locally.
 var iconYamlUrl = 'https://raw.githubusercontent.com/FortAwesome/Font-Awesome/master/src/icons.yml'
-var iconJson
+
 
 request(iconYamlUrl, function (error, response, body) {
 
   if (!error && response.statusCode === 200) {
 
     // Transform the yaml to json.
-    iconJson = yaml.parse(body)
+    var iconJson = yaml.parse(body)
 
     // Output it to the scripts directory.
     fs.writeFile('src/scripts/data.icons.json', JSON.stringify(iconJson, null, 2))
